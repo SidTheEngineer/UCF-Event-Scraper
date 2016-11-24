1d19d1042727607ec7cdd94216960dc6ef81239b
Sid <sidp@knights.ucf.edu>
2016-11-24 00:53:34 -0500

    Added days of the week (objects)  property to events_page

--- scraper.py
+++ scraper.py
@@ -4,15 +4,26 @@ import requests
 class Page(object):
     def __init__(self, url):
         self.url = url
-        self.html = self.getPageHtml()
+        self.html = self.get_page_html()
+        self.days = self.create_day_list()
     
     # Get the 'soup' object of the page via BS4.
-    def getPageHtml(self):
+    def get_page_html(self):
         response = requests.get(self.url)
         html = BeautifulSoup(response.content, 'html.parser')
         return html
 
-    def printTitles(self, html):
+    def create_day_list(self):
+        names = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+        day_list = []
+
+        for i in range(7):
+            day = Day(names[i], [])
+            day_list.append(day)
+
+        return day_list
+
+    def print_titles(self, html):
         pass
         
 class Day(object):
@@ -33,7 +44,10 @@ if __name__ == "__main__":
     events_url = "http://events.ucf.edu/this-week/"
     events_page = Page(events_url)
 
-    print(events_page.html.prettify())
+    for i in range(7):
+        print(events_page.days[i].name)
+
+    #print(events_page.html.prettify())
 
     #for event in html.findAll("li", class_="event"):
         
